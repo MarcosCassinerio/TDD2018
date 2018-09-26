d604835e27186c915d1164c58906687bf4760a26
MarcosCassinerio <nanocassi47@gmail.com>
2018-09-26 19:33:28 -0300

    arreglados errores en mazo

--- src/Mazo.php
+++ src/Mazo.php
@@ -11,32 +11,32 @@ class Mazo {
 		switch($tipo){
 			case "poker":
 				$this->cantidad = 54;
-				array_push($this->cartas), new Carta("A", 0));
-				array_push($this->cartas), new Carta("A", 1));
-				array_push($this->cartas), new Carta("A", 2));
-				array_push($this->cartas), new Carta("A", 3));
+				array_push($this->cartas, new Carta("A", 0));
+				array_push($this->cartas, new Carta("A", 1));
+				array_push($this->cartas, new Carta("A", 2));
+				array_push($this->cartas, new Carta("A", 3));
 				for($i=0;$i<36;$i++){
 					array_push($this->cartas, new Carta(($i%9)+2,i/9));
 				}
-				array_push($this->cartas), new Carta("J", 0));
-				array_push($this->cartas), new Carta("J", 1));
-				array_push($this->cartas), new Carta("J", 2));
-				array_push($this->cartas), new Carta("J", 3));
-				array_push($this->cartas), new Carta("Q", 0));
-				array_push($this->cartas), new Carta("Q", 1));
-				array_push($this->cartas), new Carta("Q", 2));
-				array_push($this->cartas), new Carta("Q", 3));
-				array_push($this->cartas), new Carta("K", 0));
-				array_push($this->cartas), new Carta("K", 1));
-				array_push($this->cartas), new Carta("K", 2));
-				array_push($this->cartas), new Carta("K", 3));
-				array_push($this->cartas), new Carta("Joker", NULL));
-				array_push($this->cartas), new Carta("Joker", NULL));
+				array_push($this->cartas, new Carta("J", 0));
+				array_push($this->cartas, new Carta("J", 1));
+				array_push($this->cartas, new Carta("J", 2));
+				array_push($this->cartas, new Carta("J", 3));
+				array_push($this->cartas, new Carta("Q", 0));
+				array_push($this->cartas, new Carta("Q", 1));
+				array_push($this->cartas, new Carta("Q", 2));
+				array_push($this->cartas, new Carta("Q", 3));
+				array_push($this->cartas, new Carta("K", 0));
+				array_push($this->cartas, new Carta("K", 1));
+				array_push($this->cartas, new Carta("K", 2));
+				array_push($this->cartas, new Carta("K", 3));
+				array_push($this->cartas, new Carta("Joker", NULL));
+				array_push($this->cartas, new Carta("Joker", NULL));
 			break;
 			case "españolas":
 				$this->cantidad = 50;
 				for($i=0;$i<48;$i++){
-					array_push($this->cartas, new Carta(($i%12)+1,$i/12))
+					array_push($this->cartas, new Carta(($i%12)+1,$i/12));
 				}
 				array_push($this->cartas, new Carta("Comodin",NULL), new Carta("Comodin",NULL));
 			break;
@@ -72,8 +72,8 @@ class Mazo {
 	}
 
 	public function cortar(){
-		$pito=rand(0,$this->cantidad);
-		for($i=0;$i<$pito;i++){
+		$rando=rand(0,$this->cantidad);
+		for($i=0;$i<$rando;$i++){
 			$this->agregar($this->obtenerCarta());
 		}
 	}
