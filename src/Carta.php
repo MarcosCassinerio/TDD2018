53b43f9ac9b14fe3513d79faacab0c8299b1f385
MarcosCassinerio <nanocassi47@gmail.com>
2018-11-25 14:02:50 -0300

    Arreglos para scrutinizer

--- src/Carta.php
+++ src/Carta.php
@@ -6,44 +6,38 @@ class Carta {
   public $palo; 
   public $numero;
   public $tipo;
-	public function __construct($palo, $numero, $tipo){
+	public function __construct($palo, $numero, $tipo) {
 		$this->palo = $palo;
 		settype($this->palo, "integer");
 		$this->numero = $numero;
 		$this->tipo = $tipo;
 	}
 
-	public function verNumero(){
+	public function verNumero() {
 		return $this->numero;
 	}
 
 	public function verPalo(){
-		switch($this->tipo){
+		switch ($this->tipo) {
 			case "poker":
-				switch($this->palo){
+				switch ($this->palo) {
 					case 0:
 					 return "Picas";
-					break;
 					case 1:
 					 return "Diamantes";
-					break;
 					case 2:
 					 return "Tréboles";
-					break;
 					case 3:
 					 return "Corazones";
 				}
 			case "españolas":
-				switch($this->palo){
+				switch ($this->palo) {
 					case 0:
 					return "Espada";
-					break;
 					case 1:
 					return "Basto";
-					break;
 					case 2:
 					return "Copa";
-					break;
 					case 3:
 					return "Oro";
 				}
--- src/Mazo.php
+++ src/Mazo.php
@@ -7,7 +7,7 @@ class Mazo {
 	protected $cartas = [];
 	public $cantidad;
 
-	public function __construct($tipo){
+	public function __construct($tipo) {
 		switch($tipo){
 			case "poker":
 				$this->cantidad = 54;
@@ -15,7 +15,7 @@ class Mazo {
 				array_push($this->cartas, new Carta("A", 1, $tipo));
 				array_push($this->cartas, new Carta("A", 2, $tipo));
 				array_push($this->cartas, new Carta("A", 3, $tipo));
-				for($i=0;$i<36;$i++){
+				for ($i=0;$i<36;$i++) {
 					array_push($this->cartas, new Carta(($i%9)+2,$i/9, $tipo));
 				}
 				array_push($this->cartas, new Carta("J", 0, $tipo));
@@ -30,50 +30,50 @@ class Mazo {
 				array_push($this->cartas, new Carta("K", 1, $tipo));
 				array_push($this->cartas, new Carta("K", 2, $tipo));
 				array_push($this->cartas, new Carta("K", 3, $tipo));
-				array_push($this->cartas, new Carta("Joker", NULL, $tipo));
-				array_push($this->cartas, new Carta("Joker", NULL, $tipo));
+				array_push($this->cartas, new Carta("Joker", null, $tipo));
+				array_push($this->cartas, new Carta("Joker", null, $tipo));
 			break;
 			case "españolas":
 				$this->cantidad = 50;
-				for($i=0;$i<48;$i++){
+				for ($i=0;$i<48;$i++) {
 					array_push($this->cartas, new Carta(($i%12)+1,$i/12, $tipo));
 				}
-				array_push($this->cartas, new Carta("Comodin",NULL, $tipo), new Carta("Comodin",NULL, $tipo));
+				array_push($this->cartas, new Carta("Comodin",null, $tipo), new Carta("Comodin",null, $tipo));
 			break;
 		}
 		$this->mezclar();
 	}
 
-	public function mezclar(){
+	public function mezclar() {
 		shuffle($this->cartas);
 	}
 
-	public function agregar($badchur){
+	public function agregar($badchur) {
 		array_push($this->cartas,$badchur);
 		$this->cantidad++;
 	}
 	
-	public function obtenerCantidad(){
+	public function obtenerCantidad() {
 		return $this->cantidad;
 	}
 
-	public function obtenerCarta(){
+	public function obtenerCarta() {
 		$this->cantidad --;
 		return array_pop($this->cartas);
 	}
 
-	public function vacio(){
-		if($this->obtenerCantidad() == 0){
-			return True;
+	public function vacio() {
+		if ($this->obtenerCantidad() == 0) {
+			return true;
 		}
-		else{
-			return False;
+		else {
+			return false;
 		}
 	}
 
-	public function cortar(){
+	public function cortar() {
 		$rando=rand(0,$this->obtenerCantidad());
-		for($i=0;$i<$rando;$i++){
+		for ($i=0;$i<$rando;$i++) {
 			$this->agregar($this->obtenerCarta());
 		}
 	}
